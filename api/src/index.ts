5e4c837802f8de5387d660548036d9b15b49be53
Lourenço Passos <lo.passos93@gmail.com>
2021-02-15 00:16:00 -0300

    chore: api layer project init

---
+++ api/src/index.ts
@@ -0,0 +1,20 @@
+import { AddressInfo } from "net";
+import express from "express";
+import { router } from "./router";
+import cors from "cors"
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.use("/api", router);
+
+export const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server running: http://localhost:${address.port}`);
+  } else {
+    console.error(`Server running failed`);
+  }
+});
